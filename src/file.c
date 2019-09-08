875f550152b6c021f2decb026c783dc8828edb55
mckenney5 <mckenneyadam@gmail.com>
2019-09-07 23:00:35 -0400

    Fixed simple mistakes

--- src/file.c
+++ src/file.c
@@ -4,12 +4,14 @@ int main(){
 	FILE *fp;
 	char page[1024] = {0};
 	fp = fopen("test.txt", "r");
-	int i;
+	if(fp == NULL){ perror(" "); return 1; }
+	size_t i;
 	for(i=0; i<1024; i++){
 		page[i] = fgetc(fp);
 		if(page[i] == EOF) break;
 	}
 	page[i] = '\0';
+	fclose(fp);
 	printf("Size: '%ld'\nText: '%s'\n", strlen(page), page);
 
 	return 0;
