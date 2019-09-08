dc01c3719b5ed839efd6465aee790e933880dccb
mckenney5 <mckenneyadam@gmail.com>
2019-09-07 23:31:05 -0400

    Made browser better

--- src/browser.c
+++ src/browser.c
@@ -1,11 +1,20 @@
+/* Does an HTTP GET request */
+/* ./a.out <website> <optional_file> */
+/* example: ./a.out index.html example.com */
 #include <stdio.h>
+#include <string.h>
+
+#define ACCEPT "text/html"
+#define USER_AGENT "netcat/1.10"
+#define ACC_LANG "en"
+
 int main(int argc, char* argv[]){
-	//if(argc != 3) return -1;
-	if(argc == 3)
-		printf("GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n", argv[1], argv[2]);
-	else if(argc == 2)
-		printf("GET / HTTP/1.0\r\nHost: %s\r\n\r\n", argv[1]);
-	else {
+	if(argc == 3 || argc == 2){
+		if(argv[2] == NULL) printf("GET / "); //stops printf for putting '(null)'
+		else printf("GET /%s ", argv[2]);
+		printf("HTTP/1.0\r\nUser-Agent: %s\r\nAccept: %s\r\nAccept-Language: %s\r\nHost: %s\r\n\r\n", 
+			USER_AGENT, ACCEPT, ACC_LANG, argv[1]);
+	} else {
 		perror("Syntax error, need to specify page and website, or just website");
 		return -1;
 	}
